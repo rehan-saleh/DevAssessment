42d920e81c9617a2986fe7741aecce4be030aff2
Rehan <rehan.siddiqui98@yahoo.com>
2019-10-28 18:43:38 +0500

    Added Material Design in Android and iOS - Added custom theme palette - Added AppTheme.xaml - Added material visual to all pages fixes #11

---
+++ src/DevAssessment/Theme/AppTheme.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DevAssessment.Theme
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppTheme : ResourceDictionary
+    {
+        public AppTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
