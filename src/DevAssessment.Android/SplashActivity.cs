ae325de701864af596917c24cb524c03f63c0e45
Rehan <rehan.siddiqui98@yahoo.com>
2019-10-28 15:40:07 +0500

    Added splash screen for Android - Added SplashActivity - Added splash_screen.xml - Added splashscreen.jpg fixes #13

---
+++ src/DevAssessment.Android/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿
+using Android.App;
+using Android.Content;
+
+namespace DevAssessment.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
