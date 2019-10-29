ef4f7cd48d66ba6ba34c03bbf9fa5c79a5729d90
Rehan <rehan.siddiqui98@yahoo.com>
2019-10-29 18:46:47 +0500

    Added Custom Dialog - Added DialogExtension - Added AlertDialogPage - Added ErrorDialogPage - Registered Alert and Error dialog pages in App.xaml.cs fixes #14

---
+++ src/DevAssessment/Extensions/DialogExtension.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Services.Dialogs;
+using System;
+
+namespace DevAssessment.Extensions
+{
+    public static class DialogExtension
+    {
+        public static void DisplayAlert(this IDialogService dialogService, string message)
+        {
+            var parameters = new DialogParameters();
+            parameters.Add("Message", message);
+
+            dialogService.ShowDialog("AlertDialogPage", parameters, CloseDialogCallback);
+        }
+
+        public static void DisplayError(this IDialogService dialogService, Exception ex, string message = null)
+        {
+            var parameters = new DialogParameters();
+
+            if (string.IsNullOrEmpty(message))
+            {
+                parameters.Add("Message", ex.Message);
+            }
+            else
+            {
+                parameters.Add("Message", message);
+            }
+
+            parameters.Add("Exception", ex);
+
+            dialogService.ShowDialog("ErrorDialogPage", parameters, CloseDialogCallback);
+        }
+
+
+        private static void CloseDialogCallback(IDialogResult dialogResult)
+        {
+
+        }
+    }
+}
